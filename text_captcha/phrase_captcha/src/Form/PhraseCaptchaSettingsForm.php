81bfe86ff0ac058fc96d99695d3fd555f13c9f73
Oleksandr Lunyov <oleksandr_lunyov@epam.com>
2018-11-17 10:52:07 +0000

    fixing code style issues in text_captcha

--- text_captcha/phrase_captcha/src/Form/PhraseCaptchaSettingsForm.php
+++ text_captcha/phrase_captcha/src/Form/PhraseCaptchaSettingsForm.php
@@ -7,7 +7,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Administration form
+ * Administration form.
  */
 class PhraseCaptchaSettingsForm extends ConfigFormBase {
 
@@ -31,7 +31,7 @@ class PhraseCaptchaSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return array('phrase_captcha.settings');
+    return ['phrase_captcha.settings'];
   }
 
   /**
@@ -40,45 +40,45 @@ class PhraseCaptchaSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('phrase_captcha.settings');
     drupal_set_message(t('WARNING: this module is not completely ported to Drupal 8 and does not work yet.'), 'warning');
-    // Radio buttons for selecting the kind of words to use
-    $form['phrase_captcha_words'] = array(
+    // Radio buttons for selecting the kind of words to use.
+    $form['phrase_captcha_words'] = [
       '#type' => 'radios',
       '#title' => t('Kind of words to use in the CAPTCHA phrase'),
-      '#options' => array(
+      '#options' => [
         PHRASE_CAPTCHA_GENERATE_NONSENSE_WORDS => t('Generate nonsense words'),
-        PHRASE_CAPTCHA_USER_DEFINED_WORDS => t('Use user defined words')
-      ),
+        PHRASE_CAPTCHA_USER_DEFINED_WORDS => t('Use user defined words'),
+      ],
       '#default_value' => $config->get('phrase_captcha_words'),
       '#required' => TRUE,
-    );
-    // Form elements for the word pools
+    ];
+    // Form elements for the word pools.
     _text_captcha_word_pool_form_items($form,
-      'phrase_captcha_userdefined_word_pool','User defined word pool',
+      'phrase_captcha_userdefined_word_pool', 'User defined word pool',
       'Enter the words to use in the CAPTCHA phrase (space separated, no punctuation).',
       ''
     );
-    // Select form element for the number of words in the CAPTCHA phrase
-    $form['phrase_captcha_word_quantity'] = array(
+    // Select form element for the number of words in the CAPTCHA phrase.
+    $form['phrase_captcha_word_quantity'] = [
       '#type' => 'select',
       '#title' => t('Number of words in the CAPTCHA phrase'),
       '#default_value' => $config->get('phrase_captcha_word_quantity'),
-      '#options' => array_combine(array(4, 5, 6, 7, 8, 9, 10), array(4, 5, 6, 7, 8, 9, 10)),
+      '#options' => array_combine([4, 5, 6, 7, 8, 9, 10], [4, 5, 6, 7, 8, 9, 10]),
       '#required' => TRUE,
-    );
-    // Select form element for the number of additional words
-    $form['phrase_captcha_additional_word_quantity'] = array(
+    ];
+    // Select form element for the number of additional words.
+    $form['phrase_captcha_additional_word_quantity'] = [
       '#type' => 'select',
       '#title' => t('Maximum number of additional words to let the user choose from'),
       '#default_value' => $config->get('phrase_captcha_additional_word_quantity'),
-      '#options' => array_combine(array(0, 1, 2, 3, 4, 5), array(0, 1, 2, 3, 4, 5)),
+      '#options' => array_combine([0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5]),
       '#required' => TRUE,
-    );
-    $form['phrase_captcha_word_selection_challenges'] = array(
+    ];
+    $form['phrase_captcha_word_selection_challenges'] = [
       '#type' => 'checkboxes',
       '#title' => t('Word selection challenges'),
       '#options' => _phrase_captcha_available_word_challenges(),
       '#default_value' => _phrase_captcha_enabled_word_challenges(),
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -110,11 +110,12 @@ class PhraseCaptchaSettingsForm extends ConfigFormBase {
       $word_count_minimum = $form_state->getValue('phrase_captcha_word_quantity') + $form_state->getValue('phrase_captcha_additional_word_quantity') + 2;
       _text_captcha_word_pool_validate('phrase_captcha_userdefined_word_pool', $form_state, $word_count_minimum, NULL, NULL);
     }
-    // Check word selection
+    // Check word selection.
     if (count(array_filter($form_state->getValue('phrase_captcha_word_selection_challenges'))) < 1) {
       $form_state->setErrorByName('phrase_captcha_word_selection_challenges', t('You need to select at least one word selection criterium'));
     }
 
     parent::validateForm($form, $form_state);
   }
+
 }
