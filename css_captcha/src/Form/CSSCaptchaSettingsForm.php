08734390157993acd28a5f9bb7c9b3e58e103631
Oleksandr Lunyov <oleksandr_lunyov@epam.com>
2018-11-17 08:56:31 +0000

    fix code issues in css_captcha

--- css_captcha/src/Form/CSSCaptchaSettingsForm.php
+++ css_captcha/src/Form/CSSCaptchaSettingsForm.php
@@ -7,7 +7,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * CSS CAPTCHA settings form
+ * CSS CAPTCHA settings form.
  */
 class CSSCaptchaSettingsForm extends ConfigFormBase {
 
@@ -31,26 +31,26 @@ class CSSCaptchaSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return array('css_captcha.settings');
+    return ['css_captcha.settings'];
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form = array();
+    $form = [];
     $config = $this->config('css_captcha.settings');
-    $form['css_captcha_allowed_characters'] = array(
+    $form['css_captcha_allowed_characters'] = [
       '#type' => 'textfield',
-      '#title' => t('Characters to use in the code'),
+      '#title' => $this->t('Characters to use in the code'),
       '#default_value' => $config->get('css_captcha_allowed_characters'),
-    );
-    $form['css_captcha_code_length'] = array(
+    ];
+    $form['css_captcha_code_length'] = [
       '#type' => 'select',
-      '#title' => t('Code length'),
-      '#options' => array_combine(array(4, 5, 6, 7, 8, 9, 10), array(4, 5, 6, 7, 8, 9, 10)),
+      '#title' => $this->t('Code length'),
+      '#options' => array_combine([4, 5, 6, 7, 8, 9, 10], [4, 5, 6, 7, 8, 9, 10]),
       '#default_value' => $config->get('css_captcha_code_length'),
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -63,7 +63,7 @@ class CSSCaptchaSettingsForm extends ConfigFormBase {
       ->set('css_captcha_allowed_characters', $form_state->getValue('css_captcha_allowed_characters'))
       ->set('css_captcha_code_length', $form_state->getValue('css_captcha_code_length'))
       ->save();
-    
+
     parent::SubmitForm($form, $form_state);
   }
 
@@ -72,9 +72,10 @@ class CSSCaptchaSettingsForm extends ConfigFormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     if (preg_match('/\s/', $form_state->getValue('css_captcha_allowed_characters'))) {
-      $form_state->setErrorByName('css_captcha_allowed_characters', t('The list of characters to use should not contain spaces.'));
+      $form_state->setErrorByName('css_captcha_allowed_characters', $this->t('The list of characters to use should not contain spaces.'));
     }
 
     parent::validateForm($form, $form_state);
   }
+
 }
