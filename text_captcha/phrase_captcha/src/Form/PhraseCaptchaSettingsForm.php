9bfda7b20c9c673c2bb87f39e31c5979bfa59969
Oleksandr Lunyov <oleksandr_lunyov@epam.com>
2018-11-17 11:48:43 +0000

    fixing code style issues in text_captcha

--- text_captcha/phrase_captcha/src/Form/PhraseCaptchaSettingsForm.php
+++ text_captcha/phrase_captcha/src/Form/PhraseCaptchaSettingsForm.php
@@ -39,14 +39,14 @@ class PhraseCaptchaSettingsForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('phrase_captcha.settings');
-    drupal_set_message(t('WARNING: this module is not completely ported to Drupal 8 and does not work yet.'), 'warning');
+    drupal_set_message($this->t('WARNING: this module is not completely ported to Drupal 8 and does not work yet.'), 'warning');
     // Radio buttons for selecting the kind of words to use.
     $form['phrase_captcha_words'] = [
       '#type' => 'radios',
-      '#title' => t('Kind of words to use in the CAPTCHA phrase'),
+      '#title' => $this->t('Kind of words to use in the CAPTCHA phrase'),
       '#options' => [
-        PHRASE_CAPTCHA_GENERATE_NONSENSE_WORDS => t('Generate nonsense words'),
-        PHRASE_CAPTCHA_USER_DEFINED_WORDS => t('Use user defined words'),
+        PHRASE_CAPTCHA_GENERATE_NONSENSE_WORDS => $this->t('Generate nonsense words'),
+        PHRASE_CAPTCHA_USER_DEFINED_WORDS => $this->t('Use user defined words'),
       ],
       '#default_value' => $config->get('phrase_captcha_words'),
       '#required' => TRUE,
@@ -60,7 +60,7 @@ class PhraseCaptchaSettingsForm extends ConfigFormBase {
     // Select form element for the number of words in the CAPTCHA phrase.
     $form['phrase_captcha_word_quantity'] = [
       '#type' => 'select',
-      '#title' => t('Number of words in the CAPTCHA phrase'),
+      '#title' => $this->t('Number of words in the CAPTCHA phrase'),
       '#default_value' => $config->get('phrase_captcha_word_quantity'),
       '#options' => array_combine([4, 5, 6, 7, 8, 9, 10], [4, 5, 6, 7, 8, 9, 10]),
       '#required' => TRUE,
@@ -68,14 +68,14 @@ class PhraseCaptchaSettingsForm extends ConfigFormBase {
     // Select form element for the number of additional words.
     $form['phrase_captcha_additional_word_quantity'] = [
       '#type' => 'select',
-      '#title' => t('Maximum number of additional words to let the user choose from'),
+      '#title' => $this->t('Maximum number of additional words to let the user choose from'),
       '#default_value' => $config->get('phrase_captcha_additional_word_quantity'),
       '#options' => array_combine([0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5]),
       '#required' => TRUE,
     ];
     $form['phrase_captcha_word_selection_challenges'] = [
       '#type' => 'checkboxes',
-      '#title' => t('Word selection challenges'),
+      '#title' => $this->t('Word selection challenges'),
       '#options' => _phrase_captcha_available_word_challenges(),
       '#default_value' => _phrase_captcha_enabled_word_challenges(),
     ];
@@ -112,7 +112,7 @@ class PhraseCaptchaSettingsForm extends ConfigFormBase {
     }
     // Check word selection.
     if (count(array_filter($form_state->getValue('phrase_captcha_word_selection_challenges'))) < 1) {
-      $form_state->setErrorByName('phrase_captcha_word_selection_challenges', t('You need to select at least one word selection criterium'));
+      $form_state->setErrorByName('phrase_captcha_word_selection_challenges', $this->t('You need to select at least one word selection criterium'));
     }
 
     parent::validateForm($form, $form_state);
